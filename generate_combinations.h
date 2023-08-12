a21ba80f4b973859aa676c5f24be4870b5c4c549
Dvir Cohen <dvirdc@gmail.com>
2023-08-12 20:33:50 +0300

    Update generate_combinations.h

--- generate_combinations.h
+++ generate_combinations.h
@@ -12,8 +12,7 @@ void md5_round2(const unsigned int *M, const unsigned int *in, unsigned int *out
 void md5_round3(const unsigned int *M, const unsigned int *in, unsigned int *out);
 void md5_round4(const unsigned int *M, const unsigned int *in, unsigned int *out);
 void md5(const unsigned int *in, unsigned int out[4], const size_t size);
-//void generate_combinations(const unsigned char md5bf[16], const unsigned char charset[CHARSET_LENGTH], int charset_size, unsigned char out[4]);
-void generate_combinations(const unsigned char md5bf[16], const unsigned char charset[CHARSET_LENGTH], unsigned char out[4]);
+void generate_combinations(const unsigned char md5bf[32], const unsigned char charset[CHARSET_LENGTH], unsigned char out[4]);
 ap_uint<32> factorial(ap_uint<8> n);
 // Function to compute permutations of n items taken r at a time.
 ap_uint<32> permutation(ap_uint<8> n, ap_uint<8> r);
