fadc2d05956981c12b6a38536f244937376f24d0
Dvir Cohen <dvirdc@gmail.com>
2023-08-18 11:11:16 +0300

    Update test_generate_combinations.cpp

--- test_generate_combinations.cpp
+++ test_generate_combinations.cpp
@@ -1,5 +1,11 @@
 #include "generate_combinations.h"
 
+//void print_md5(unsigned char* md5);
+//
+//void print_md5(unsigned char* md5) {
+//    for(unsigned char* p=md5; p<md5+16; ++p) printf("%02x",*p);
+//    printf("\n");
+//}
 
 void print_arr(unsigned char* arr, int length)
 {
@@ -9,14 +15,23 @@ void print_arr(unsigned char* arr, int length)
 
 int main()
 {
-    const unsigned char charset[] = "abc2";
-    int length = 4;
-    unsigned char out[4];
+    const int LEN = LENGTH;
+//	const unsigned char charset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@";
+	const unsigned char charset[] = "AabCcdefghijklmnopqrstuvwxyz01234567";
+
+//	const unsigned char charset[] = "abcdef";
+
+    unsigned char out[LEN];
     const unsigned char md5bf[] = "47bce5c74f589f4867dbd57e9ca9f808"; // hashed "aaa"
-    const unsigned char md5bf_2[] = "bcbe3365e6ac95ea2c0343a2395834dd"; // "cca"
-    generate_combinations(md5bf_2, charset, out);
-    for (int i = 0; i < 4; i++) {
+    const unsigned char md5bf_2[] = "5a89924ad353f0f0d5b7bf6ee8e0909b"; // "cca"
+    const unsigned char md5bf_3[] = "43b5688a505003ebae686e46c866eca7"; // "AbCdE5!"
+    const unsigned char md5bf_4[] = "4506ca152a692ca44a41998450f583f4"; // AbCd
+    printf("calling generate_combinations!\n");
+    generate_combinations(md5bf_4, charset, out);
+    printf("result: \n");
+    for (int i = 0; i < LEN; i++) {
 		printf("%c", out[i]);
 	}
+    printf("\n");
     return 0;
 }
